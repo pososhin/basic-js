c0aac1f9119bccf3a7e73660cd59cb6b94323265
Посошин Дмитрий <C:\Users\dp\AppData\Roaming\The Bat!>
2022-04-29 15:27:03 +0300

    feat: encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let ch = "";
+  let count = 0;
+  let ret = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ch) {
+      if (count > 0) ret += (count > 1 ? count.toString() : "") + ch;
+      ch = str[i];
+      count = 1;
+    } else {
+      count += 1;
+    }
+  }
+  return ret + (count > 1 ? count.toString() : "") + ch;
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
