76084b3c960a485cd34930723677b7515c6ded24
Посошин Дмитрий <C:\Users\dp\AppData\Roaming\The Bat!>
2022-04-29 15:32:14 +0300

    feat: get-email-domain

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disks,speed) {
+  const turns = 2**disks-1;
+  return { turns: turns, seconds: Math.floor(turns*3600/speed), }
 }
 
 module.exports = {
