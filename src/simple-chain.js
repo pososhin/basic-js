766ae3b4c7905c21ff4aaeaa89221eb085efb2d9
Посошин Дмитрий <C:\Users\dp\AppData\Roaming\The Bat!>
2022-04-29 15:46:29 +0300

    feat: sort-by-height

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,4 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
-/**
- * Implement chainMaker object according to task description
- * 
- */
-// const chainMaker = {
-//   getLength() {
-//     throw new NotImplementedError('Not implemented');
-//     // remove line with error and write your code here
-//   },
-//   addLink(/* value */) {
-//     throw new NotImplementedError('Not implemented');
-//     // remove line with error and write your code here
-//   },
-//   removeLink(/* position */) {
-//     throw new NotImplementedError('Not implemented');
-//     // remove line with error and write your code here
-//   },
-//   reverseChain() {
-//     throw new NotImplementedError('Not implemented');
-//     // remove line with error and write your code here
-//   },
-//   finishChain() {
-//     throw new NotImplementedError('Not implemented');
-//     // remove line with error and write your code here
-//   }
-// };
-
 class ChainMaker {
   constructor() {
     this.chain = [];
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function sortByHeight(arr) {
+  let sort_arr = arr.filter((x) => x != -1).sort((a,b)=>a-b);
+  for(let i=0,j=0;i<arr.length;i++){
+    if(arr[i]==-1) continue;
+    arr[i]=sort_arr[j];
+    j++;
+  }
+  return arr;
 }
 
 module.exports = {
