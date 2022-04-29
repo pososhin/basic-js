67f1be3b6c727acaf44d54c8a1cb6d9e10e72204
Посошин Дмитрий <C:\Users\dp\AppData\Roaming\The Bat!>
2022-04-29 15:39:53 +0300

    feat: recursive-depth

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,10 +23,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper (m) {
+  if(!m[0] || m[0].length<1) return 0;
+  let count = 0;
+  let zerro = Array(m.length)
+  for(let i=0;i<zerro.length;i++) zerro[i]= [...m[i]].fill(0);
+  const fn_set=(r,c)=>{
+    if(r<0 || r>=m.length) return ;
+    if(c<0 || c>=m[0].length) return ;
+    zerro[r][c]++;
+  }
+  for (let i = 0; i < m.length; i++) 
+    for (let j = 0; j < m[i].length; j++) {
+      if(!m[i][j]) continue ;
+      fn_set(i-1,j-1);
+      fn_set(i-1,j);
+      fn_set(i-1,j+1);
+      fn_set(i,j-1);
+      fn_set(i,j+1);
+      fn_set(i+1,j-1);
+      fn_set(i+1,j);
+      fn_set(i+1,j+1);
+  }
+  return zerro;
 }
+
 
 module.exports = {
   minesweeper
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    if(!Array.isArray(arr)) return 0;
+    return 1+arr.reduce((max,x)=>{
+        const cd =this.calculateDepth(x);
+        return (cd>max)?cd:max; 
+      },0)
   }
 }
 
